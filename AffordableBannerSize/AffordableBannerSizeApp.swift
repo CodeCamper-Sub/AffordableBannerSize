a4a8691a9a8d13d6d7c4fca5b2d321d598b9e068
Eric/한경수 <gksrudtn99@gmail.com>
2024-05-20 11:49:07 +0900

    Initial Commit

---
+++ AffordableBannerSize/AffordableBannerSizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffordableBannerSizeApp.swift
+//  AffordableBannerSize
+//
+//  Created by 한경수 on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AffordableBannerSizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
